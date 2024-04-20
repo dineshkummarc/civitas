ede00f34c4a5a753e72832b39fc0d844d0c719da
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-04-20 07:55:08 -0400

    Show a list of posts on the home page

--- topics/list.php
+++ topics/list.php
@@ -1,6 +1,25 @@
 <?php
 $title = "Browsing posts";
 include "../layout/top.php";
+include "../db.php";
+
+$sql = "SELECT `topics`.*, `users`.`username` FROM `topics`
+JOIN `users` ON `topics`.`creator_id` = `users`.`id`
+ORDER BY `last_reply` DESC";
+
+$topics = $conn->execute_query($sql)->fetch_all(MYSQLI_ASSOC);
 ?>
+
+<?php foreach($topics as $topic): ?>
+    <div>
+        <header>
+            <a href="/topics/view.php?id=<?= $topic["id"] ?>">
+                <?= htmlspecialchars($topic["title"]) ?>
+            </a>
+        </header>
+        By @<?= htmlspecialchars($topic["username"]) ?>
+        • Last reply: <?= htmlspecialchars($topic["last_reply"]) ?>
+    </div>
+<?php endforeach ?>
 
 <?php include "../layout/bottom.php" ?>
